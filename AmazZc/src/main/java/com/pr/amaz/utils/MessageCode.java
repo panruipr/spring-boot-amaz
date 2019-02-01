5bbf4533631230d4900d2cf309aee88744703020
phfundWork <panrui1993616@gmail.com>
2019-02-01 17:07:05 +0800

    spring-boot集成redis

---
+++ AmazZc/src/main/java/com/pr/amaz/utils/MessageCode.java
@@ -0,0 +1,10 @@
+package com.pr.amaz.utils;
+
+public class MessageCode {
+    //成功
+    public String SUCCESSFUL = "000000";
+
+    //session为空(登录超时)
+    public String SESSION_TIMEOUT = "000001";
+
+}
